815fc111aaf61630fbc0e7a75a1712396da96ab9
Nikorexio <81244446+Nikorexio@users.noreply.github.com>
2022-06-13 15:33:12 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,19 +4,19 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--languages', action='store', default=None,
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru/en/etc")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
-    language = request.config.getoption("languages")
+    language = request.config.getoption("language")
     if language is not None:
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--languages should be typed")
+        raise pytest.UsageError("--language should be typed")
     yield browser
     print("\nquit browser..")
     browser.quit()
